232cf6360afe619134946d0628a8b974228151f3
AlcidesJr <8090502+alcidestbj@users.noreply.github.com>
2024-08-14 23:17:45 -0300

    Add files via upload

---
+++ exemplo1.c
@@ -0,0 +1,37 @@
+/*
+* Programa: Impressão de texto na tela
+* Descrição: Este programa demonstra a estrutura básica
+*            de um programa em C e faz a impressão de uma mensagem na tela.
+* Autor: Prof Alcides
+* Data: Agosto/2024
+* Versão: 1.0
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    printf("Meu primeiro programa em C!");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
